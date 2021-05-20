ff0296e3906fc98769f62e6b1e25a2d2c2f2f5e8
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2021-05-20 17:13:48 +0200

    Update index.php
    Co-Authored-By: Stephane Sob Fouodji <stefan-kruger7@hotmail.com>

--- public/staff/pages/index.php
+++ public/staff/pages/index.php
@@ -3,20 +3,19 @@
 <?php
 
   require_login();
-
-  $page_set = find_all_pages();
-
+//  $page_set = find_all_pages();
+  redirect_to('staff/index.php');
 ?>
-
-<?php $page_title = 'Pages'; ?>
-<?php include(SHARED_PATH . '/staff_header.php'); ?>
+<!--
+<?php /*$page_title = 'Pages'; */?>
+<?php /*include(SHARED_PATH . '/staff_header.php'); */?>
 
 <div id="content">
   <div class="pages listing">
     <h1>Pages</h1>
 
     <div class="actions">
-      <a class="action" href="<?php echo url_for('/staff/pages/new.php'); ?>">Create New Page</a>
+      <a class="action" href="<?php /*echo url_for('/staff/pages/new.php'); */?>">Create New Page</a>
     </div>
 
   	<table class="list">
@@ -31,25 +30,25 @@
         <th>&nbsp;</th>
   	  </tr>
 
-      <?php while($page = mysqli_fetch_assoc($page_set)) { ?>
-        <?php $subject = find_subject_by_id($page['subject_id']); ?>
+      <?php /*while($page = mysqli_fetch_assoc($page_set)) { */?>
+        <?php /*$subject = find_subject_by_id($page['subject_id']); */?>
         <tr>
-          <td><?php echo h($page['id']); ?></td>
-          <td><?php echo h($subject['menu_name']); ?></td>
-          <td><?php echo h($page['position']); ?></td>
-          <td><?php echo $page['visible'] == 1 ? 'true' : 'false'; ?></td>
-    	    <td><?php echo h($page['menu_name']); ?></td>
-          <td><a class="action" href="<?php echo url_for('/staff/pages/show.php?id=' . h(u($page['id']))); ?>">View</a></td>
-          <td><a class="action" href="<?php echo url_for('/staff/pages/edit.php?id=' . h(u($page['id']))); ?>">Edit</a></td>
-          <td><a class="action" href="<?php echo url_for('/staff/pages/delete.php?id=' . h(u($page['id']))); ?>">Delete</a></td>
+          <td><?php /*echo h($page['id']); */?></td>
+          <td><?php /*echo h($subject['menu_name']); */?></td>
+          <td><?php /*echo h($page['position']); */?></td>
+          <td><?php /*echo $page['visible'] == 1 ? 'true' : 'false'; */?></td>
+    	    <td><?php /*echo h($page['menu_name']); */?></td>
+          <td><a class="action" href="<?php /*echo url_for('/staff/pages/show.php?id=' . h(u($page['id']))); */?>">View</a></td>
+          <td><a class="action" href="<?php /*echo url_for('/staff/pages/edit.php?id=' . h(u($page['id']))); */?>">Edit</a></td>
+          <td><a class="action" href="<?php /*echo url_for('/staff/pages/delete.php?id=' . h(u($page['id']))); */?>">Delete</a></td>
     	  </tr>
-      <?php } ?>
+      <?php /*} */?>
   	</table>
 
-    <?php mysqli_free_result($page_set); ?>
+    <?php /*mysqli_free_result($page_set); */?>
 
   </div>
 
 </div>
 
-<?php include(SHARED_PATH . '/staff_footer.php'); ?>
+--><?php /*include(SHARED_PATH . '/staff_footer.php'); */?>
